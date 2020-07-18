1e70bb1b13f3f70599b4b5323b8f0d1744a664b6
AshredCR <55962947+EdCastillo@users.noreply.github.com>
2020-07-18 15:33:29 -0600

    SucursalController Update

--- src/main/java/com/techstore/web/controller/SucursalController.java
+++ src/main/java/com/techstore/web/controller/SucursalController.java
@@ -1,10 +1,6 @@
 package com.techstore.web.controller;
 import com.techstore.web.dao.SucursalRepository;
-import com.techstore.web.dao.UsuarioRepository;
-import com.techstore.web.model.RolUsuario;
 import com.techstore.web.model.Sucursal;
-import com.techstore.web.model.Usuario;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
