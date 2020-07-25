730b6cd84acd07b7155995ba9754dbf7f2f9c392
Jose Mora <jdevmora@gmail.com>
2020-07-25 01:58:15 -0600

    Creacion del CRUD de usuarios

--- src/main/java/com/techstore/web/controller/UsuarioController.java
+++ src/main/java/com/techstore/web/controller/UsuarioController.java
@@ -1,15 +1,17 @@
 package com.techstore.web.controller;
 
+import com.techstore.web.dao.RolUsuarioRepository;
 import com.techstore.web.dao.UsuarioRepository;
 import com.techstore.web.model.Usuario;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,13 +22,58 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private RolUsuarioRepository rolUsuarioRepository;
+
     @GetMapping("/")
     public ModelAndView listarUsuarios(ModelMap model){
-        // Buscamos todos los usuarios de la BD
         List<Usuario> listaUsuarios = usuarioRepository.findAll();
-        //Asignamos la lista a un atributo del modelo que va a ver Thymeleaf
         model.addAttribute("listaUsuarios", listaUsuarios);
-        // Retornamos la vista con el modelo
         return new ModelAndView("usuarios/listar-usuarios", model);
+    }
+    
+    @GetMapping("/crear")
+    public ModelAndView crearUsuario(ModelMap model){
+        model.addAttribute("usuario", new Usuario());
+        model.addAttribute("listaRoles", rolUsuarioRepository.findAll());
+        model.addAttribute("modo", "crear");
+        return new ModelAndView("usuarios/editar-usuario", model);
+    }
+
+    @PostMapping(value = "/crear")
+    public ModelAndView guardarNuevoUsuario(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, RedirectAttributes redirectAttrs) {
+        usuarioRepository.save(usuario);
+        redirectAttrs.addFlashAttribute("mensaje", "Usuario creado exitosamente");
+        return new ModelAndView("redirect:/usuarios/");
+    }
+
+    @GetMapping("/{nombreUsuario}")
+    public ModelAndView mostrarUsuario(@PathVariable String nombreUsuario, ModelMap model){
+        Usuario usuario = usuarioRepository.findByNombreUsuario(nombreUsuario);
+        model.addAttribute("usuario", usuario);
+        return new ModelAndView("usuarios/ver-usuario", model);
+    }
+
+    @GetMapping("/editar/{nombreUsuario}")
+    public ModelAndView editarUsuario(@PathVariable String nombreUsuario, ModelMap model){
+        Usuario usuario = usuarioRepository.findByNombreUsuario(nombreUsuario);
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("listaRoles", rolUsuarioRepository.findAll());
+        model.addAttribute("modo", "editar");
+        return new ModelAndView("usuarios/editar-usuario", model);
+    }
+
+    @PostMapping(value = "/editar/{nombreUsuario}")
+    public ModelAndView guardarEditarUsuario(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, RedirectAttributes redirectAttrs) {
+        usuarioRepository.save(usuario);
+        redirectAttrs.addFlashAttribute("mensaje", "Usuario actualizado exitosamente");
+        return new ModelAndView("redirect:/usuarios/");
+    }
+
+    @GetMapping("/eliminar/{nombreUsuario}")
+    public ModelAndView eliminarUsuario(@PathVariable String nombreUsuario, RedirectAttributes redirectAttrs){
+        usuarioRepository.deleteByNombreUsuario(nombreUsuario);
+        redirectAttrs.addFlashAttribute("mensaje", "Usuario eliminado exitosamente");
+        return new ModelAndView("redirect:/usuarios/");
     }
 }
--- src/main/java/com/techstore/web/dao/UsuarioRepository.java
+++ src/main/java/com/techstore/web/dao/UsuarioRepository.java
@@ -4,6 +4,13 @@ import com.techstore.web.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByNombreUsuario(String nombreUsuario);
+
+    @Transactional
+    void deleteByNombreUsuario(String nombreUsuario);
 }
