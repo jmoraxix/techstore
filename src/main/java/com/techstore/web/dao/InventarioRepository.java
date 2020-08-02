85eec327eb37d97020f641222af378d5fcbed280
Maikmm30 <56731399+Maikmm30@users.noreply.github.com>
2020-08-02 10:06:08 -0600

    Se agrego el CRUD,controller, model, repository, y html's, de Inventario

---
+++ src/main/java/com/techstore/web/dao/InventarioRepository.java
@@ -0,0 +1,9 @@
+package com.techstore.web.dao;
+
+import com.techstore.web.model.Inventario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventarioRepository extends JpaRepository<Inventario, Long> {
+}
