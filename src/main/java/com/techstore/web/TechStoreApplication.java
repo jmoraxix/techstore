6121327d8c80bb2356a13d21b3154f571edf4049
Maikmm30 <56731399+Maikmm30@users.noreply.github.com>
2020-08-09 17:44:51 -0600

    Se creo el controlador, servicio, y html para subir imagen, y se modifico el Main, y el template

--- src/main/java/com/techstore/web/TechStoreApplication.java
+++ src/main/java/com/techstore/web/TechStoreApplication.java
@@ -1,13 +1,26 @@
 package com.techstore.web;
 
+import com.techstore.web.storage.StorageProperties;
+import com.techstore.web.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class TechStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechStoreApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
---
+++ src/main/java/com/techstore/web/controller/FileUploadController.java
@@ -0,0 +1,62 @@
+package com.techstore.web.controller;
+
+import com.techstore.web.storage.StorageFileNotFoundException;
+import com.techstore.web.storage.StorageService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@Log4j2
+@RequestMapping("/subirimagen")
+public class FileUploadController {
+
+	private final StorageService storageService;
+
+	@Autowired
+	public FileUploadController(StorageService storageService) {
+		this.storageService = storageService;
+	}
+
+	@GetMapping("/")
+	public ModelAndView listUploadedFiles(Model model) throws IOException {
+		model.addAttribute("files", storageService.loadAll().map(path
+				-> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,"serveFile",
+				path.getFileName().toString()).build().toUri().toString())
+				.collect(Collectors.toList()));
+		return new ModelAndView("uploadForm", (Map<String, ?>) model);
+	}
+
+	@GetMapping("/files/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+		Resource file = storageService.loadAsResource(filename);
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+	}
+
+	@PostMapping("/")
+	public ModelAndView handleFileUpload(@RequestParam("file") MultipartFile file,RedirectAttributes redirectAttributes) {
+		storageService.store(file);
+		redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
+		return new ModelAndView("redirect:/subirimagen/");
+	}
+
+	@ExceptionHandler(StorageFileNotFoundException.class)
+	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+		return ResponseEntity.notFound().build();
+	}
+
+}
